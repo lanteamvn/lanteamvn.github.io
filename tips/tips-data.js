ef0517501b4ec20e677e1095f0cfe8780e94f9bd
LANTeam Project <128953242+lanteamvn@users.noreply.github.com>
2025-04-13 13:16:12 +0700

    LAN Bot --sc : ./autocommit with id priv

---
+++ tips/tips-data.js
@@ -0,0 +1,99 @@
+export const tips = [
+    {
+      id: "cach-doi-mat-khau-windows-linux-macos",
+      title: "Mẹo: Đổi mật khẩu Windows, Linux, macOS",
+      shortDesc: "Hướng dẫn chi tiết cách đổi mật khẩu trên từng hệ điều hành bằng Terminal! ",
+      content: `
+        <h3>Windows</h3>
+        <code>net user [tên người dùng] [mật khẩu mới]</code>
+  
+        <h2>Linux/Ubuntu/macOS</h2>
+        <p><code>passwd</code></p>
+        <p>Nhập mật khẩu hiện tại và sau đó nhập mật khẩu mới.</p>
+        <p>Nếu bạn muốn đổi cho một người dùng khác, hãy dùng: <code>sudo passwd [tên người dùng]</code>.</p>
+  
+      `,
+      author: "kudodz ✅",
+      date: "13/04/2025, 11:44 AM - UTC+7",
+      os: "Windows, Linux, macOS"
+    },
+
+    {
+      id: "cac-link-tai-cac-ung-dung",
+      title: "Các Link Tải Các Ứng Dụng",
+      shortDesc: "Cách link tải các ứng dụng tổng hợp (FAST)",
+      content: `
+        <h2>⚠️ LƯU Ý: CÁC NGUỒN LINK ĐƯỢC TỔNG HỢP TỪ NHIỀU NƠI KHÁC NHAU, NÊN CÓ THỂ KHÔNG ĐÚNG HOÀN TOÀN.</h2>
+        <h2>contact me: <a href="https://t.me/nozilaz">@nozilaz</a></h2>
+        <h3>Các Link</h3>
+        <p>Scratch: <a href="https://scratch.mit.edu/download">https://scratch.mit.edu/download</a></p>
+        <p>Visual Studio Code: <a href="https://code.visualstudio.com/download">https://code.visualstudio.com/download</a></p>
+        <p>Google Chrome: <a href="https://www.google.com/intl/vi_ALL/chrome/">https://www.google.com/intl/vi_ALL/chrome/</a></p>
+        <p>Mozilla Firefox: <a href="https://www.mozilla.org/vi/firefox/new/">https://www.mozilla.org/vi/firefox/new/</a></p>
+        <p>Zoom: <a href="https://zoom.us/download">https://zoom.us/download</a></p>
+        <p>Microsoft Teams: <a href="https://www.microsoft.com/en-us/microsoft-teams/download-app">https://www.microsoft.com/en-us/microsoft-teams/download-app</a></p>
+        <p>Discord: <a href="https://discord.com/download">https://discord.com/download</a></p>
+        <p>Spotify: <a href="https://www.spotify.com/vn/download">https://www.spotify.com/vn/download</a></p>
+        <p>Adobe Reader: <a href="https://get.adobe.com/reader/">https://get.adobe.com/reader/</a></p>
+        <p>Adobe Photoshop: <a href="https://www.adobe.com/products/photoshop/free-trial-download.html">https://www.adobe.com/products/photoshop/free-trial-download.html</a></p>
+        <p>Adobe Illustrator: <a href="https://www.adobe.com/products/illustrator/free-trial-download.html">https://www.adobe.com/products/illustrator/free-trial-download.html</a></p>
+        <p>Adobe After Effects: <a href="https://www.adobe.com/products/aftereffects/free-trial-download.html">https://www.adobe.com/products/aftereffects/free-trial-download.html</a></p>
+        <p>Adobe Premiere Pro: <a href="https://www.adobe.com/products/premiere/free-trial-download.html">https://www.adobe.com/products/premiere/free-trial-download.html</a></p>
+        <p>Adobe XD: <a href="https://www.adobe.com/products/xd/free-trial-download.html">https://www.adobe.com/products/xd/free-trial-download.html</a></p>
+        <p>Adobe Lightroom: <a href="https://www.adobe.com/products/photoshop-lightroom/free-trial-download.html">https://www.adobe.com/products/photoshop-lightroom/free-trial-download.html</a></p>
+        <p>Adobe InDesign: <a href="https://www.adobe.com/products/indesign/free-trial-download.html">https://www.adobe.com/products/indesign/free-trial-download.html</a></p>
+        <p>Adobe Dreamweaver: <a href="https://www.adobe.com/products/dreamweaver/free-trial-download.html">https://www.adobe.com/products/dreamweaver/free-trial-download.html</a></p>
+        <p>Adobe Animate: <a href="https://www.adobe.com/products/animate/free-trial-download.html">https://www.adobe.com/products/animate/free-trial-download.html</a></p>
+        <p>Adobe Audition: <a href="https://www.adobe.com/products/audition/free-trial-download.html">https://www.adobe.com/products/audition/free-trial-download.html</a></p>
+        <p>Adobe Acrobat Pro: <a href="https://www.adobe.com/products/acrobat/free-trial-download.html">https://www.adobe.com/products/acrobat/free-trial-download.html</a></p>
+        <p>Adobe Spark: <a href="https://spark.adobe.com/">https://spark.adobe.com/</a></p>
+        <p>Adobe Fresco: <a href="https://www.adobe.com/products/fresco.html">https://www.adobe.com/products/fresco.html</a></p>
+        <p>Adobe Dimension: <a href="https://www.adobe.com/products/dimension.html">https://www.adobe.com/products/dimension.html</a></p>
+        <p>Adobe Character Animator: <a href="https://www.adobe.com/products/character-animator.html">https://www.adobe.com/products/character-animator.html</a></p>
+        <p>Adobe Media Encoder: <a href="https://www.adobe.com/products/media-encoder.html">https://www.adobe.com/products/media-encoder.html</a></p>
+        <p>Adobe Prelude: <a href="https://www.adobe.com/products/prelude.html">https://www.adobe.com/products/prelude.html</a></p>
+        <p>Adobe Bridge: <a href="https://www.adobe.com/products/bridge.html">https://www.adobe.com/products/bridge.html</a></p>
+        <p>Adobe Scout: <a href="https://www.adobe.com/products/scout.html">https://www.adobe.com/products/scout.html</a></p>
+        <p>Adobe Captivate: <a href="https://www.adobe.com/products/captivate.html">https://www.adobe.com/products/captivate.html</a></p>
+        <p>Adobe Connect: <a href="https://www.adobe.com/products/adobeconnect.html">https://www.adobe.com/products/adobeconnect.html</a></p>
+        <p>Adobe Presenter: <a href="https://www.adobe.com/products/presenter.html">https://www.adobe.com/products/presenter.html</a></p>
+        <p>Adobe Captivate Prime: <a href="https://www.adobe.com/products/captivateprime.html">https://www.adobe.com/products/captivateprime.html</a></p>
+        <p>Adobe Experience Design: <a href="https://www.adobe.com/products/xd.html">https://www.adobe.com/products/xd.html</a></p>
+        <p>Adobe Experience Manager: <a href="https://www.adobe.com/experience-manager.html">https://www.adobe.com/experience-manager.html</a></p>
+        <p>Adobe Campaign: <a href="https://www.adobe.com/campaign.html">https://www.adobe.com/campaign.html</a></p>
+        <p>Adobe Target: <a href="https://www.adobe.com/target.html">https://www.adobe.com/target.html</a></p>
+        <p>Adobe Analytics: <a href="https://www.adobe.com/analytics.html">https://www.adobe.com/analytics.html</a></p>
+        <p>Adobe Audience Manager: <a href="https://www.adobe.com/audience-manager.html">https://www.adobe.com/audience-manager.html</a></p>
+        <p>Adobe Experience Platform: <a href="https://www.adobe.com/experience-platform.html">https://www.adobe.com/experience-platform.html</a></p>
+        <p>Adobe Sensei: <a href="https://www.adobe.com/sensei.html">https://www.adobe.com/sensei.html</a></p>
+        <p>Adobe Stock: <a href="https://stock.adobe.com/">https://stock.adobe.com/</a></p>
+        <p>Adobe Fonts: <a href="https://fonts.adobe.com/">https://fonts.adobe.com/</a></p>
+        <p>Adobe Color: <a href="https://color.adobe.com/">https://color.adobe.com/</a></p>
+        <p>Adobe Spark Post: <a href="https://spark.adobe.com/post/">https://spark.adobe.com/post/</a></p>
+        <p>Adobe Spark Page: <a href="https://spark.adobe.com/page/">https://spark.adobe.com/page/</a></p>
+        <p>Adobe Spark Video: <a href="https://spark.adobe.com/video/">https://spark.adobe.com/video/</a></p>
+        <p>Adobe Spark Video Maker: <a href="https://spark.adobe.com/video-maker/">https://spark.adobe.com/video-maker/</a></p>
+        <p>Adobe Spark Animation: <a href="https://spark.adobe.com/animation/">https://spark.adobe.com/animation/</a></p>
+        <p>Adobe Spark Animation Maker: <a href="https://spark.adobe.com/animation-maker/">https://spark.adobe.com/animation-maker/</a></p>
+        <p>Adobe Spark Animation Creator: <a href="https://spark.adobe.com/animation-creator/">https://spark.adobe.com/animation-creator/</a></p>
+        <p>Adobe Spark Animation Tool: <a href="https://spark.adobe.com/animation-tool/">https://spark.adobe.com/animation-tool/</a></p>
+        <p>Adobe Spark Animation Software: <a href="https://spark.adobe.com/animation-software/">https://spark.adobe.com/animation-software/</a></p>
+        <p>Adobe Spark Animation App: <a href="https://spark.adobe.com/animation-app/">https://spark.adobe.com/animation-app/</a></p>
+        <p>Adobe Spark Animation Program: <a href="https://spark.adobe.com/animation-program/">https://spark.adobe.com/animation-program/</a></p>
+        <p>Adobe Spark Animation Platform: <a href="https://spark.adobe.com/animation-platform/">https://spark.adobe.com/animation-platform/</a></p>
+        <p>Adobe Spark Animation Service: <a href="https://spark.adobe.com/animation-service/">https://spark.adobe.com/animation-service/</a></p>
+        <p>Adobe Spark Animation Solution: <a href="https://spark.adobe.com/animation-solution/">https://spark.adobe.com/animation-solution/</a></p>
+        <p>Adobe Spark Animation Toolset: <a href="https://spark.adobe.com/animation-toolset/">https://spark.adobe.com/animation-toolset/</a></p>
+        <p>Adobe Spark Animation Toolkit: <a href="https://spark.adobe.com/animation-toolkit/">https://spark.adobe.com/animation-toolkit/</a></p>
+        <p>Adobe Spark Animation Library: <a href="https://spark.adobe.com/animation-library/">https://spark.adobe.com/animation-library/</a></p>
+        <p>Adobe Spark Animation Resources: <a href="https://spark.adobe.com/animation-resources/">https://spark.adobe.com/animation-resources/</a></p>
+        <p>Adobe Spark Animation Community: <a href="https://spark.adobe.com/animation-community/">https://spark.adobe.com/animation-community/</a></p>
+        <p>Adobe Spark Animation Forum: <a href="https://spark.adobe.com/animation-forum/">https://spark.adobe.com/animation-forum/</a></p>
+      
+      `,
+      author: "nozilaz ✅",
+      date: "13/04/2025, 11:44 AM - UTC+7",
+      os: "Windows, Linux, macOS"
+    }
+  ];
+  
